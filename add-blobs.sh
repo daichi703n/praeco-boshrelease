87e56bfbdab8dc8e0e97751b56f32c67aae1ed7d
Daichi Kimura <daichi703n@gmail.com>
2019-03-27 01:24:17 +0900

    Add shell for add-blob

---
+++ add-blobs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR=`pwd`
+
+mkdir -p .downloads
+
+cd .downloads
+
+blob_download() {
+  set -eu
+  local package=$1
+  local url=$2
+  local f=$3
+  if [ ! -f ${DIR}/blobs/${package}/${f} ];then
+    curl -L -J ${url} -o ${f}
+    bosh add-blob --dir=${DIR} ${f} ${package}/${f}
+  fi
+}
+
+blob_download node https://nodejs.org/dist/v10.15.3/node-v10.15.3.tar.gz node-v10.15.3.tar.gz
