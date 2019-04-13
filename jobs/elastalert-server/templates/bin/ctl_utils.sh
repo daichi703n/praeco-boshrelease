908100533a20f46e41ce234036c1b21713d43ac9
Daichi Kimura <daichi703n@gmail.com>
2019-04-13 20:20:03 +0900

    [WIP] Fix stop script for kill child process

--- jobs/elastalert-server/templates/bin/ctl_utils.sh
+++ jobs/elastalert-server/templates/bin/ctl_utils.sh
@@ -164,6 +164,40 @@ function kill_and_wait() {
   fi
 }
 
+function kill_and_wait_pid() {
+  declare pidfile="$1" pid="$2" timeout="${2:-25}" sigkill_on_timeout="${3:-1}"
+
+  if [ ! "${pid}" ]; then
+    echo "PID doesn't exist"
+    exit 0
+  fi
+
+  if ! pid_is_running "${pid}"; then
+    echo "Process ${pid} is not running"
+    rm -f "${pidfile}"
+    exit 0
+  fi
+
+  echo "Killing ${pid} "
+  kill "${pid}"
+
+  if ! wait_pid_death "${pid}" "${timeout}"; then
+    if [ "${sigkill_on_timeout}" = "1" ]; then
+      echo "Kill timed out, using kill -SIGINT on ${pid}"
+      kill -9 "${pid}"
+      sleep 0.5
+    fi
+  fi
+
+  if pid_is_running "${pid}"; then
+    echo "Timed Out"
+    exit 1
+  else
+    echo "Stopped"
+    rm -f "${pidfile}"
+  fi
+}
+
 check_nfs_mount() {
   opts=$1
   exports=$2
