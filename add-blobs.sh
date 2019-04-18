5714ded2b35c93ab8e8a38692bf1d7b861b945ab
Daichi Kimura <daichi703n@gmail.com>
2019-04-19 01:06:39 +0900

    [WIP] Temporary succeed to run praeco GUI

--- add-blobs.sh
+++ add-blobs.sh
@@ -33,10 +33,30 @@ blob_npm_download() {
   fi
 }
 
+blob_git_npm_download() {
+  set -eu
+  local package=$1
+  local url=$2
+  local f=$3
+  if [ ! -f ${DIR}/blobs/${package}/${f} ];then
+    if [ ! -d ${package} ];then
+      git clone ${url}
+    fi
+    pushd ${package}
+      git pull
+      npm install
+    popd
+    tar zcf ${f} ${package}
+    bosh add-blob --dir=${DIR} ${f} ${package}/${f}
+  fi
+}
+
 blob_download node https://nodejs.org/dist/v10.15.3/node-v10.15.3-linux-x64.tar.xz node-v10.15.3.tar.xz
-blob_npm_download elastalert-server https://github.com/ServerCentral/elastalert-server/archive/1.0.0.tar.gz elastalert-server-1.0.0.tar.gz
+# blob_npm_download elastalert-server https://github.com/ServerCentral/elastalert-server/archive/1.0.0.tar.gz elastalert-server-1.0.0.tar.gz
 blob_download elastalert https://github.com/Yelp/elastalert/archive/v0.1.39.tar.gz elastalert-v0.1.39.tar.gz
 blob_npm_download praeco https://github.com/ServerCentral/praeco/archive/v0.3.4.tar.gz praeco-0.3.4.tar.gz
+
+blob_git_npm_download elastalert-server https://github.com/ServerCentral/elastalert-server elastalert-server-1.0.2.tar.gz
 
 blob_download python2.7 https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz Python-2.7.15.tgz
 blob_download libffi https://buildpacks.cloudfoundry.org/dependencies/manual-binaries/python/libffi-3.2.1-linux-x64-5f5bf32c.tgz libffi-3.2.1.tgz
--- jobs/praeco/templates/bin/pre-start.sh
+++ jobs/praeco/templates/bin/pre-start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "praeco"
+CONFIG_DIR=/var/vcap/jobs/praeco/config
+PACKAGE_DIR=/var/vcap/packages/praeco
+
+ln -fs $CONFIG_DIR/api.config.json $PACKAGE_DIR/config/api.config.json
