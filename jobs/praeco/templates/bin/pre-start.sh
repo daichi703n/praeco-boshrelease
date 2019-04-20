7e17c32355ca20676b9dea0e0e11a43c4a575fae
Daichi Kimura <daichi703n@gmail.com>
2019-04-21 02:34:38 +0900

    [WIP] Succeeded tocreate rule on praeco, w/elastalert bump 0.1.35 -> 0.1.39 (missing argument on py script)

--- jobs/praeco/templates/bin/pre-start.sh
+++ jobs/praeco/templates/bin/pre-start.sh
@@ -2,5 +2,10 @@
 
 CONFIG_DIR=/var/vcap/jobs/praeco/config
 PACKAGE_DIR=/var/vcap/packages/praeco
+ELASTALERT_PACKAGE_DIR=/var/vcap/packages/elastalert
 
 ln -fs $CONFIG_DIR/api.config.json $PACKAGE_DIR/config/api.config.json
+
+mkdir $ELASTALERT_PACKAGE_DIR/rules
+mkdir $ELASTALERT_PACKAGE_DIR/rule_templates
+ln -fs $CONFIG_DIR/BaseRule.config $ELASTALERT_PACKAGE_DIR/rules/BaseRule.config
