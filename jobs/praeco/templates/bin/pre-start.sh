e25dfd29578ac9bfc2b551b036669b436bdb06fb
Daichi Kimura <daichi703n@gmail.com>
2019-05-06 17:17:06 +0900

    [WIP] Temporary succeed to alerting

--- jobs/praeco/templates/bin/pre-start.sh
+++ jobs/praeco/templates/bin/pre-start.sh
@@ -2,10 +2,18 @@
 
 CONFIG_DIR=/var/vcap/jobs/praeco/config
 PACKAGE_DIR=/var/vcap/packages/praeco
+STORE_DIR=/var/vcap/store/praeco
+ELASTALERT_JOB_DIR=/var/vcap/jobs/elastalert
 ELASTALERT_PACKAGE_DIR=/var/vcap/packages/elastalert
 
 ln -fs $CONFIG_DIR/api.config.json $PACKAGE_DIR/config/api.config.json
 
+#TODO: if not exist
 mkdir $ELASTALERT_PACKAGE_DIR/rules
-mkdir $ELASTALERT_PACKAGE_DIR/rule_templates
+mkdir $STORE_DIR/rules
+#TODO: if not exist
+# mkdir $ELASTALERT_PACKAGE_DIR/rule_templates
+mkdir $STORE_DIR/rule_templates
 ln -fs $CONFIG_DIR/BaseRule.config $ELASTALERT_PACKAGE_DIR/rules/BaseRule.config
+ln -fs $STORE_DIR/rules $ELASTALERT_JOB_DIR/rules/praeco_rules
+ln -fs $STORE_DIR/rule_templates $ELASTALERT_JOB_DIR/rules/praeco_rule_templates
--- manifests/deploy-praeco.sh
+++ manifests/deploy-praeco.sh
@@ -2,4 +2,5 @@ bosh deploy -d praeco manifests/praeco.yml \
   -v slack_webhook_url=https://hooks.slack.com/services/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 \
   -o manifests/ops-files/praeco-slack-config.yml \
   -v praeco_slack_webhook_url=https://hooks.slack.com/services/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 \
-  --no-redact
+  --no-redact \
+  -o manifests/ops-files/elastalert-rule-monitor-error-log.yml
