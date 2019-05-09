3ae581305327814d4693c03d4fea18b1d7a6c4bc
Daichi Kimura <daichi703n@gmail.com>
2019-05-09 13:07:13 +0900

    fix path of BasuRule.config

--- jobs/praeco/templates/bin/pre-start.sh
+++ jobs/praeco/templates/bin/pre-start.sh
@@ -13,9 +13,9 @@ mkdir $ELASTALERT_PACKAGE_DIR/rules
 mkdir $STORE_DIR
 mkdir $STORE_DIR/rules
 mkdir $STORE_DIR/rule_templates
-ln -fns $CONFIG_DIR/BaseRule.config $ELASTALERT_PACKAGE_DIR/rules/BaseRule.config
+ln -fns $CONFIG_DIR/BaseRule.config $STORE_DIR/rules/BaseRule.config
 # ln -fns $STORE_DIR/rules $ELASTALERT_JOB_DIR/rules/praeco_rules
 # ln -fns $STORE_DIR/rule_templates $ELASTALERT_JOB_DIR/rules/praeco_rule_templates
 
 ln -fns $CONFIG_DIR/praeco.config.json $PACKAGE_DIR/public/praeco.config.json
-ln -fns $ELASTALERT_JOB_DIR/rules $STORE_DIR/rules/static_rules
+# ln -fns $ELASTALERT_JOB_DIR/rules $STORE_DIR/rules/static_rules
