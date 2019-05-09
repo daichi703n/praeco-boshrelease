569ff003f807e696b8026cec5600307894a7fd96
Daichi Kimura <daichi703n@gmail.com>
2019-05-09 13:06:49 +0900

    [WIP] Update params for elastic-stack

--- jobs/elastalert-server/templates/bin/pre-start.sh
+++ jobs/elastalert-server/templates/bin/pre-start.sh
@@ -3,8 +3,8 @@
 CONFIG_DIR=/var/vcap/jobs/elastalert-server/config
 PACKAGE_DIR=/var/vcap/packages/elastalert-server
 
-ln -fs $CONFIG_DIR/config.json $PACKAGE_DIR/config/config.json
-# ln -fs $PACKAGE_DIR/config/elastalert.yaml $PACKAGE_DIR/config.yaml
-ln -fs $CONFIG_DIR/config.yaml /var/vcap/packages/elastalert/config.yaml
+ln -fns $CONFIG_DIR/config.json $PACKAGE_DIR/config/config.json
+# ln -fns $PACKAGE_DIR/config/elastalert.yaml $PACKAGE_DIR/config.yaml
+ln -fns $CONFIG_DIR/config.yaml /var/vcap/packages/elastalert/config.yaml
 
-ln -fs /var/vcap/packages/python2.7/lib/libpython2.7.so.1.0 /lib/x86_64-linux-gnu/
+ln -fns /var/vcap/packages/python2.7/lib/libpython2.7.so.1.0 /lib/x86_64-linux-gnu/
--- jobs/praeco/templates/bin/pre-start.sh
+++ jobs/praeco/templates/bin/pre-start.sh
@@ -6,15 +6,16 @@ STORE_DIR=/var/vcap/store/praeco
 ELASTALERT_JOB_DIR=/var/vcap/jobs/elastalert
 ELASTALERT_PACKAGE_DIR=/var/vcap/packages/elastalert
 
-# ln -fs $CONFIG_DIR/api.config.json $PACKAGE_DIR/config/api.config.json
+# ln -fns $CONFIG_DIR/api.config.json $PACKAGE_DIR/config/api.config.json
 
 #TODO: if not exist
-# mkdir $ELASTALERT_PACKAGE_DIR/rules
+mkdir $ELASTALERT_PACKAGE_DIR/rules
+mkdir $STORE_DIR
 mkdir $STORE_DIR/rules
 mkdir $STORE_DIR/rule_templates
-ln -fs $CONFIG_DIR/BaseRule.config $ELASTALERT_SERVER_STORE_DIR/rules/BaseRule.config
-# ln -fs $STORE_DIR/rules $ELASTALERT_JOB_DIR/rules/praeco_rules
-# ln -fs $STORE_DIR/rule_templates $ELASTALERT_JOB_DIR/rules/praeco_rule_templates
+ln -fns $CONFIG_DIR/BaseRule.config $ELASTALERT_PACKAGE_DIR/rules/BaseRule.config
+# ln -fns $STORE_DIR/rules $ELASTALERT_JOB_DIR/rules/praeco_rules
+# ln -fns $STORE_DIR/rule_templates $ELASTALERT_JOB_DIR/rules/praeco_rule_templates
 
-ln -fs $CONFIG_DIR/praeco.config.json $PACKAGE_DIR/public/praeco.config.json
-ln -fs $ELASTALERT_JOB_DIR/rules $STORE_DIR/rules/static_rules
+ln -fns $CONFIG_DIR/praeco.config.json $PACKAGE_DIR/public/praeco.config.json
+ln -fns $ELASTALERT_JOB_DIR/rules $STORE_DIR/rules/static_rules
